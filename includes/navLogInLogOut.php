bcd05ae96e3a3763f79d5b20024eaac17d1df2fc
Marcus Sauceda <marcussauceda@Marcuss-iMac.local>
2022-01-14 11:53:57 -0600

    edited nav file

--- includes/navLogInLogOut.php
+++ includes/navLogInLogOut.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a href="https://socialnetwork-lamp.herokuapp.com/login.php/showAllPosts.php" class="navbar-brand">Chatter</a>
+        <a href="https://socialnetwork-lamp.herokuapp.com/showAllPosts.php" class="navbar-brand">Chatter</a>
         <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -8,12 +8,12 @@
             <div class="navbar-nav">
                 <!-- <a href="blog.php" class="nav-item nav-link">Blog</a> -->
                 <!-- <a href="posts.php" class="nav-item nav-link">Posts</a> -->
-                <a href="https://socialnetwork-lamp.herokuapp.com/login.php/showAllPosts.php" class="nav-item nav-link">Posts</a>
+                <a href="https://socialnetwork-lamp.herokuapp.com/showAllPosts.php" class="nav-item nav-link">Posts</a>
                 <?php echo $msg2 ?>
                 <!-- <a href="blogArchive.php" class="nav-item nav-link">Archives</a> -->
             </div>
             <div class="navbar-nav ms-auto">
-                <a href="https://socialnetwork-lamp.herokuapp.com/login.php/login.php" class="nav-item nav-link"><?php echo $msg ?></a>
+                <a href="https://socialnetwork-lamp.herokuapp.com/login.php" class="nav-item nav-link"><?php echo $msg ?></a>
             </div>
         </div>
     </div>
